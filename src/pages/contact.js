338db935a92b0ddef5b2347d5648824c15dfa568
nyasharonaldo <nyasharonaldo@gmail.com>
2020-02-09 12:28:57 +0000

    Initial Commit

---
+++ src/pages/contact.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Contact Page</h1>
+        <p>
+          {" "}
+          <strong>Name: </strong> Nyasha{" "}
+        </p>
+        <p>
+          {" "}
+          <strong>Address: </strong> 2100 Suck Ya Mum Boulevard{" "}
+        </p>
+        <p>
+          {" "}
+          <strong>Email: </strong> nmronaldo@gmail.com{" "}
+        </p>
+        <p>
+          {" "}
+          <strong>Number: </strong> 07841377640{" "}
+        </p>
+        <p>
+          {" "}
+          <strong>Research: </strong>
+          <a href="https://www.google.com" target="_blank">
+            Click me
+          </a>{" "}
+        </p>
+      </Layout>
+    </div>
+  )
+}
+
+export default ContactPage
