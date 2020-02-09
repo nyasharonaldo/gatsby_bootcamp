62d0528d78f211a487e08c074c3e76beec27d283
nyasharonaldo <nyasharonaldo@gmail.com>
2020-02-09 16:24:35 +0000

    Commit of contenful implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     author: "Nyasha Ronaldo Mutangadura",
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options:{
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,33 +1,46 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
-import blogStyles from './blog.module.scss'
+import blogStyles from "./blog.module.scss"
 const BlogPage = () => {
-  const data = useStaticQuery(graphql`
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     allMarkdownRemark {
+  //       edges {
+  //         node {
+  //           frontmatter {
+  //             title
+  //             date
+  //           }
+  //           fields{
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  const dataContentful = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields{
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do YYYY")
           }
         }
       }
     }
   `)
-  
-  console.log(data)
+
   return (
     <div>
       <Layout>
         <h1>Blog</h1>
         <ol className={blogStyles.posts}>
-          {data.allMarkdownRemark.edges.map((edge)=> {
+          {/* {data.allMarkdownRemark.edges.map((edge)=> {
             return(
               <li className={blogStyles.post}>
                 <Link to={'/blog/'+edge.node.fields.slug}>
@@ -36,10 +49,18 @@ const BlogPage = () => {
                 </Link>
               </li>
             )            
+          })} */}
+          {dataContentful.allContentfulBlogPost.edges.map(edge => {
+            return (
+              <li className={blogStyles.post}>
+                <Link to={"/blog/" + edge.node.slug}>
+                  <h2>{edge.node.title}</h2>
+                  <p>{edge.node.publishedDate}</p>
+                </Link>
+              </li>
+            )
           })}
         </ol>
-
-        
       </Layout>
     </div>
   )
